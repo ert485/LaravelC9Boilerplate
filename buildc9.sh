7e512a3e422d09b20c1f71b170f3478e0368d934
Erik Tetland <ert485@mail.usask.ca>
2017-11-08 06:58:48 +0000

    fixes to build.sh

--- buildc9.sh
+++ buildc9.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Update to php7.1
+
+# Update to php7.1
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/ondrej-php-trusty.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 sudo apt install -y libapache2-mod-php7.1
@@ -9,24 +10,35 @@ sudo service apache2 restart
 sudo apt install -y php7.1-dom
 sudo apt install -y php7.1-mbstring
 sudo apt install -y php7.1-zip
+sudo apt-get install -y php7.1-mysql
 
+# Make project
 composer require "laravel/installer"
 PATH=$PATH:$PWD/vendor/bin
 laravel new tempLaravel
 rm -rf vendor
 mv tempLaravel/* tempLaravel/.* .
+echo ".c9" >> .gitignore
 
+# default string length bug fix
+#sed -i "N;/boot()\\n    {/a\\\t\\tSchema::defaultStringLength(191);" app/Providers/AppServiceProvider.php
+sed -i "N;N;/boot()\\n    {/a\\\t\\tSchema::defaultStringLength(191);" app/Providers/AppServiceProvider.php  
 
-#Host from public folder only
-#copy site .conf file
+# Edit environment file
+sed -i "/DB_DATABASE=/c\DB_DATABASE=c9" ~/workspace/.env
+sed -i "/DB_USERNAME=/c\DB_USERNAME=$C9_USER" ~/workspace/.env
+sed -i "/DB_PASSWORD=/c\DB_PASSWORD=" ~/workspace/.env
+
+# Host from public folder only
 sudo cp /etc/apache2/sites-available/001-cloud9.conf /etc/apache2/sites-available/002-laravel.conf
 #change the site to be hosted from the "public" folder
 sudo perl -pi -w -e 's/\/home\/ubuntu\/workspace/\/home\/ubuntu\/workspace\/public/g;' /etc/apache2/sites-available/002-laravel.conf
 #set the correct sites enabled 
 sudo a2dissite 000*; sudo a2dissite 001*; sudo a2ensite 002*
 
-#Start apache server (in background)
+# Start apache server (in background)
 run-apache2 &
 echo "echo hosting at http://\$C9_PROJECT-\$C9_USER.c9users.io" > site
 chmod 750 site
+# Display site location
 ./site
